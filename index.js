bf0d2f525f20bdd018932a95d7abdb1da9692769
bmnazmussakib <nazmussakib161@gmail.com>
2022-02-15 03:07:25 +0600

    server created and DB CONNECTED

---
+++ index.js
@@ -0,0 +1,56 @@
+// Express JS
+const express = require('express');
+const app = express();
+
+// port
+const PORT = 1010;
+
+
+// Cors
+const cors = require('cors');
+app.use(cors());
+
+
+// Body Parser
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+// MySQL
+const mysql = require('mysql');
+
+// Database Config
+const databaseConnectionConfig = {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'todo'
+}
+
+
+const conn = mysql.createConnection(databaseConnectionConfig);
+
+conn.connect((error) => {
+    if (error) {
+        console.log("Connection Failed");
+        console.log(error);
+    } else {
+        console.log("Connection Successful");
+    }
+    
+});
+
+
+
+
+
+
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
